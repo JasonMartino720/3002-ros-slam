983f3a134e0b1537fee7d85645a07f7327e60fbf
Kohmei Kadoya <kkadoya@wpi.edu>
2020-11-03 17:44:15 -0500

    most of lab 1 done
    not ec

--- rbe3002_lab2/src/nodes/lab2.py
+++ rbe3002_lab2/src/nodes/lab2.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python2
 
+import math
 import rospy
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Vector3
 from tf.transformations import euler_from_quaternion
 
 class Lab2:
@@ -14,17 +16,17 @@ class Lab2:
         """
         ### REQUIRED CREDIT
         ### Initialize node, name it 'lab2'
-        rospy.init_node('node1', anonymous=True) 
+        rospy.init_node('lab2', anonymous=True)
         ### Tell ROS that this node publishes Twist messages on the '/cmd_vel' topic
-        # TODO
+        self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+
         ### Tell ROS that this node subscribes to Odometry messages on the '/odom' topic
         ### When a message is received, call self.update_odometry
-        # TODO
+        rospy.Subscriber("/odom", Odometry, self.update_odometry())
+
         ### Tell ROS that this node subscribes to PoseStamped messages on the '/move_base_simple/goal' topic
         ### When a message is received, call self.go_to
-        # TODO
-        pass
-
+        rospy.Subscriber("/move_base_simple/goal", PoseStamped, self.go_to())
 
 
     def send_speed(self, linear_speed, angular_speed):
@@ -35,23 +37,36 @@ class Lab2:
         """
         ### REQUIRED CREDIT
         ### Make a new Twist message
-        # TODO
+        msg_cmd_vel = Twist()
+        linear = Vector3()
+        linear.x = linear_speed
+        linear.y = 0
+        linear.z = 0
+        angular = Vector3()
+        angular.x = 0
+        angular.y = 0
+        angular.z = angular_speed
+        msg_cmd_vel.linear = linear
+        msg_cmd_vel.angular = angular
         ### Publish the message
-        # TODO
-        pass # delete this when you implement your code
+        self.pub.publish(msg_cmd_vel)
 
-    
-        
     def drive(self, distance, linear_speed):
         """
         Drives the robot in a straight line.
         :param distance     [float] [m]   The distance to cover.
         :param linear_speed [float] [m/s] The forward linear speed.
         """
-        ### REQUIRED CREDIT
-        pass # delete this when you implement your code
+        tolerance = 0.005 #meters
 
+        self.ix = self.px
+        self.iy = self.py
+        self.send_speed(linear_speed,0)
 
+        while(self.dist_between(self.ix,self.iy,self.px,self.py) < distance - tolerance):
+            rospy.sleep(0.005)
+
+        self.send_speed(0,0)
 
     def rotate(self, angle, aspeed):
         """
@@ -59,10 +74,18 @@ class Lab2:
         :param angle         [float] [rad]   The distance to cover.
         :param angular_speed [float] [rad/s] The angular speed.
         """
-        ### REQUIRED CREDIT
-        pass # delete this when you implement your code
+        tolerance = 0.005 #meters
 
+        self.ith = self.pth
+        self.send_speed(0, aspeed)
 
+        while(self.pth < self.ith + angle - tolerance):
+            rospy.sleep(0.005)
+
+        self.send_speed(0, 0)
+
+    def dist_between(self,ix,iy,x,y):
+        return math.sqrt((ix-x * ix-x) + (iy-y * iy-y))
 
     def go_to(self, msg):
         """
@@ -70,10 +93,17 @@ class Lab2:
         This method is a callback bound to a Subscriber.
         :param msg [PoseStamped] The target pose.
         """
-        ### REQUIRED CREDIT
-        pass # delete this when you implement your code
 
+        ix = msg.point.x
+        iy = msg.point.y
+        self.rotate(math.atan((py-iy)/(px-ix)))
 
+        self.drive(dist_between(msg.point.x,msg.point.y),0.1)
+
+        quat_orig = msg.quaternion
+        quat_list = [quat_orig.x, quat_orig.y, quat_orig.z, quat_orig.w]
+        (roll , pitch , yaw) = euler_from_quaternion(quat_list)
+        self.rotate(yaw, 0.1)
 
     def update_odometry(self, msg):
         """
@@ -81,9 +111,12 @@ class Lab2:
         This method is a callback bound to a Subscriber.
         :param msg [Odometry] The current odometry information.
         """
-        ### REQUIRED CREDIT
-        # TODO
-        pass # delete this when you implement your code
+        self.px = msg.pose.pose.position.x
+        self.py = msg.pose.pose.position.y
+        quat_orig = msg.pose.pose.orientation
+        quat_list = [quat_orig.x, quat_orig.y, quat_orig.z, quat_orig.w]
+        (roll , pitch , yaw) = euler_from_quaternion(quat_list)
+        self.pth = yaw
 
 
 
