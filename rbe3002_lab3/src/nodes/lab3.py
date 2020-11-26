67ef8675b57ce9cd19068d1b66c036d79c149c7c
Jason Martino <jamartino@wpi.edu>
2020-11-26 00:08:51 -0500

    Half written new PID code

--- rbe3002_lab3/src/nodes/lab3.py
+++ rbe3002_lab3/src/nodes/lab3.py
@@ -42,19 +42,18 @@ class Lab3:
         msg_to_send = GetPlan()
         curr_pos = PoseStamped()
 
-        #Creating A PoseStamped msg of the current robot position for GetPlan.start
+        # Creating A PoseStamped msg of the current robot position for GetPlan.start
         curr_pos.pose.position = Point(self.px, self.py, 0)
         quat = quaternion_from_euler(0, 0, self.pth)
         curr_pos.pose.orientation = Quaternion(quat[0], quat[1], quat[2], quat[3])
         # rospy.loginfo("Creating Msg w/ CurPos: " + str(curr_pos))
         # rospy.loginfo("Creating Msg w/ Goal: " + str(msg))
 
-        #Request Plan
+        # Request Plan
         path_planner = rospy.ServiceProxy('plan_path', GetPlan)
         get_plan_obj = path_planner(curr_pos, msg, TOLERANCE)
 
-
-        #publihs plan
+        # publihs plan
         # Path Visualization
         self.pathPub.publish(get_plan_obj.plan)
         rospy.loginfo(get_plan_obj.plan)
@@ -97,7 +96,7 @@ class Lab3:
         :param distance     [float] [m]   The distance to cover.
         :param linear_speed [float] [m/s] The forward linear speed.
         """
-        TOLERANCE = 0.0005 #meters
+        TOLERANCE = 0.0005  # meters
         Kp = 2.140
         Ki = 0.05
         Kd = 4
@@ -113,37 +112,37 @@ class Lab3:
         # while(dist_between(self.ix,self.iy,self.px,self.py) < distance - TOLERANCE):
         #     rospy.sleep(0.005)
         #     self.send_speed(linear_speed, 0)
-        error = dist_between(self.goal.x,self.goal.y,self.ix,self.iy) - dist_between(self.ix, self.iy, self.px, self.py)
+        error = dist_between(self.goal.x, self.goal.y, self.ix, self.iy) - dist_between(self.ix, self.iy, self.px,
+                                                                                        self.py)
         lastError = 0
         intergral = 0
         # while True:
-        while(abs(error) > TOLERANCE):
-            if(self.newOdomReady2):
+        while (abs(error) > TOLERANCE):
+            if (self.newOdomReady2):
                 self.newOdomReady2 = False
-                error = dist_between(self.goal.x,self.goal.y,self.ix,self.iy) - dist_between(self.ix, self.iy, self.px, self.py)
+                error = dist_between(self.goal.x, self.goal.y, self.ix, self.iy) - dist_between(self.ix, self.iy,
+                                                                                                self.px, self.py)
 
                 intergral += error
                 intergral = max(-1, min(intergral, 1))
                 derivative = error - lastError
                 lastError = error
 
-                pidOutput = (Kp*error) + (Ki*intergral) + (Kd*derivative)
+                pidOutput = (Kp * error) + (Ki * intergral) + (Kd * derivative)
 
                 clamped = max(-linear_speed, min(pidOutput, linear_speed))
                 self.send_speed(clamped, -self.angular_z)
-                rospy.loginfo('The target pos is %f, %f we are currently at %f, %f error %f int %f derivative %f clamped is %f' % (
-                self.goal.x, self.goal.y, self.px, self.py, error, intergral, derivative, clamped))
-
+                rospy.loginfo(
+                    'The target pos is %f, %f we are currently at %f, %f error %f int %f derivative %f clamped is %f' % (
+                        self.goal.x, self.goal.y, self.px, self.py, error, intergral, derivative, clamped))
 
         # for x in range(1000):
         #     rospy.sleep(0.005)
         #     self.send_speed(linear_speed*((1000-x)/1000.0),0)
 
-        self.send_speed(0,0)
-
+        self.send_speed(0, 0)
 
         rospy.loginfo("Move Done!")
-
 
     def rotate(self, angle, aspeed):
         """
@@ -151,9 +150,9 @@ class Lab3:
         :param angle         [float] [rad]   The distance to cover.
         :param angular_speed [float] [rad/s] The angular speed.
         """
-        TOLERANCE = 0.0001 #rad
+        TOLERANCE = 0.0001  # rad
 
-        goalAngle = normalize_angle(angle+math.pi)
+        goalAngle = normalize_angle(angle + math.pi)
         # goalAngle = angle+math.pi
 
         # self.send_speed(0, aspeed)
@@ -161,14 +160,13 @@ class Lab3:
         Ki = -1
         Kd = -6
 
-
         integral = 0
         lastError = 0
 
         error = self.pth - goalAngle
         while abs(error) > TOLERANCE:
-        # while True:
-            if(self.newOdomReady):
+            # while True:
+            if (self.newOdomReady):
                 self.newOdomReady = False
                 error = self.pth - goalAngle
 
@@ -178,15 +176,50 @@ class Lab3:
                 derivative = error - lastError
                 lastError = error
 
-                pidOutput = (Kp*error) + (Ki*integral) + (Kd*derivative)
+                pidOutput = (Kp * error) + (Ki * integral) + (Kd * derivative)
 
                 clamped = max(-aspeed, min(pidOutput, aspeed))
                 self.send_speed(0, clamped)
-                rospy.loginfo('The target orientation is %f we are currently at %f the error is %f, the intergeral is %f, deriviate is %f -> clamped is %f' %(
-                goalAngle, self.pth, error, integral, derivative,clamped))
+                rospy.loginfo(
+                    'The target orientation is %f we are currently at %f the error is %f, the intergeral is %f, deriviate is %f -> clamped is %f' % (
+                        goalAngle, self.pth, error, integral, derivative, clamped))
 
         rospy.loginfo("Rotate Done!")
         self.send_speed(0, 0)
+
+    def drive_and_turn(self, linear_speed, goal_msg, target_angle, angular_speed):
+        drive_kP = 0.0
+        drive_kI = 0.0
+        drive_kD = 0.0
+        turn_kP = 0.0
+        turn_kI = 0.0
+        turn_kD = 0.0
+
+        drive_TOLERANCE = 0.0005
+        turn_TOLERANCE = 0.0001
+
+        curr_abs_angle = self.absolute_angle(bool(target_angle > 0))
+
+        drive_error = dist_between(goal_msg.x, goal_msg.y, self.px, self.py)
+        turn_error = curr_abs_angle - target_angle
+
+        drive_lastError = 0
+        drive_integral = 0
+        turn_lastError = 0
+        turn_integral = 0
+
+    def absolute_angle(self, positive):
+        if positive:
+            if self.pth < 0:
+                return self.pth + math.radians(360)
+            else:
+                return self.pth
+        else:
+            if self.pth > 0:
+                return self.pth - math.radians(360)
+            else:
+                return self.pth
+
 
     def go_to(self, msg):
         """
@@ -195,20 +228,20 @@ class Lab3:
         :param msg [PoseStamped] The target pose.
         """
 
-        #From: https://emanual.robotis.com/docs/en/platform/turtlebot3/specifications/
-        ROTATION_SPEED = 1.34 #Rad/sec
-        DRIVE_SPEED = 0.22 #Meters/sec
+        # From: https://emanual.robotis.com/docs/en/platform/turtlebot3/specifications/
+        ROTATION_SPEED = 1.34  # Rad/sec
+        DRIVE_SPEED = 0.22  # Meters/sec
 
         self.goal = msg.pose.position
         # rospy.loginfo("Curr Pos: " + str(self.px) + " AND " + str(self.py))
         # rospy.loginfo("Goal Pos: " + str(goal.x) + " AND " + str(goal.y))
 
-        rospy.loginfo("Going to intital angle: "+ str(angle_to_goal(self.px,self.py,self.goal.x,self.goal.y)))
-        self.rotate(angle_to_goal(self.px,self.py,self.goal.x,self.goal.y), ROTATION_SPEED)
+        rospy.loginfo("Going to intital angle: " + str(angle_to_goal(self.px, self.py, self.goal.x, self.goal.y)))
+        self.rotate(angle_to_goal(self.px, self.py, self.goal.x, self.goal.y), ROTATION_SPEED)
         rospy.loginfo("initial ended at this angle: " + str(self.pth))
         rospy.sleep(0.5)
-        rospy.loginfo("Going distance of: " + str(dist_between(self.px,self.py,self.goal.x,self.goal.y)))
-        self.drive(dist_between(self.px,self.py,self.goal.x,self.goal.y), DRIVE_SPEED)
+        rospy.loginfo("Going distance of: " + str(dist_between(self.px, self.py, self.goal.x, self.goal.y)))
+        self.drive(dist_between(self.px, self.py, self.goal.x, self.goal.y), DRIVE_SPEED)
         rospy.sleep(0.5)
         # # rospy.loginfo("Going to final angle: " + str(orientation_to_yaw(msg.pose.orientation)))
         # # self.rotate(orientation_to_yaw(msg.pose.orientation), ROTATION_SPEED)
@@ -234,12 +267,12 @@ class Lab3:
         :param msg [PoseStamped] The target pose.
         """
 
-        TOLERANCE = 0.1 #meters
+        TOLERANCE = 0.1  # meters
 
         goal = msg.pose.position
 
-        self.send_speed(0.22, solve_arc_omega(self.px,self.py,self.pth,goal.x,goal.y))
-        while(dist_between(self.px,self.py,goal.x,goal.y) >  TOLERANCE):
+        self.send_speed(0.22, solve_arc_omega(self.px, self.py, self.pth, goal.x, goal.y))
+        while (dist_between(self.px, self.py, goal.x, goal.y) > TOLERANCE):
             rospy.sleep(0.005)
         print("Arc Done!")
 
@@ -250,35 +283,35 @@ class Lab3:
         :param linear_speed [float] [m/s] The maximum forward linear speed.
         """
 
-        RAMP_DOWN = 0.5 #meters
+        RAMP_DOWN = 0.5  # meters
 
         self.ix = self.px
         self.iy = self.py
 
         for x in range(1000):
             rospy.sleep(0.005)
-            self.send_speed(linear_speed*(x/1000.0),0)
-        self.send_speed(linear_speed,0)
+            self.send_speed(linear_speed * (x / 1000.0), 0)
+        self.send_speed(linear_speed, 0)
 
-        while(dist_between(self.ix,self.iy,self.px,self.py) < distance - RAMP_DOWN):
+        while (dist_between(self.ix, self.iy, self.px, self.py) < distance - RAMP_DOWN):
             rospy.sleep(0.005)
 
         for x in range(1000):
             rospy.sleep(0.005)
-            self.send_speed(linear_speed*((1000-x)/1000.0),0)
+            self.send_speed(linear_speed * ((1000 - x) / 1000.0), 0)
 
-        self.send_speed(0,0)
-
+        self.send_speed(0, 0)
 
     def run(self):
         rospy.spin()
 
 
-def dist_between(ix,iy,x,y):
+def dist_between(ix, iy, x, y):
     """
     Get the magnitude of the displacement between two points
     """
-    return math.sqrt(((ix-x) * (ix-x)) + ((iy-y) * (iy-y)))
+    return math.sqrt(((ix - x) * (ix - x)) + ((iy - y) * (iy - y)))
+
 
 def normalize_angle(angle):
     """
@@ -287,12 +320,13 @@ def normalize_angle(angle):
     :param angle the input angle
     """
     finalAngle = angle
-    while(finalAngle > math.pi):
-        finalAngle -= math.pi*2
-    while(finalAngle < -math.pi):
-        finalAngle += math.pi*2
-    print('Input: %f | Output: %f',(angle, finalAngle))
+    while (finalAngle > math.pi):
+        finalAngle -= math.pi * 2
+    while (finalAngle < -math.pi):
+        finalAngle += math.pi * 2
+    print('Input: %f | Output: %f', (angle, finalAngle))
     return finalAngle
+
 
 def orientation_to_yaw(orientation):
     """
@@ -300,43 +334,45 @@ def orientation_to_yaw(orientation):
     """
     quat_orig = orientation
     quat_list = [quat_orig.x, quat_orig.y, quat_orig.z, quat_orig.w]
-    (roll , pitch , yaw) = euler_from_quaternion(quat_list)
+    (roll, pitch, yaw) = euler_from_quaternion(quat_list)
     return yaw
 
-def solve_turn_dir(current_angle,goal_angle):
+
+def solve_turn_dir(current_angle, goal_angle):
     """
     Takes angles between -pi -> pi and tells you which way to turn
     -1 means CCW and 1 mean CW
     """
     diff = goal_angle - current_angle
-    if(diff < 0):
+    if (diff < 0):
         diff += math.pi
-    if(diff > math.pi/2):
-        return 1 # left turn
+    if (diff > math.pi / 2):
+        return 1  # left turn
     else:
-        return -1 # right turn
+        return -1  # right turn
 
-def angle_to_goal(curr_x,curr_y,goal_x,goal_y):
+
+def angle_to_goal(curr_x, curr_y, goal_x, goal_y):
     """
     Find the angle of the goal w.r.t to current position
     """
     return normalize_angle(math.atan2((curr_y - goal_y), (curr_x - goal_x)))
 
 
-
-def solve_arc_omega(curr_x,curr_y,curr_theta,goal_x,goal_y):
+def solve_arc_omega(curr_x, curr_y, curr_theta, goal_x, goal_y):
     """
     Solve for the omega required for the arc
     """
-    arc_triangle_theta = (math.pi / 2) - (angle_to_goal(curr_x,curr_y,goal_x,goal_y)-curr_theta)
-    #Solve for the angle of the right trangle between arc and normal or robot
-    a = dist_between(curr_x,curr_y,goal_x,goal_y)/2
+    arc_triangle_theta = (math.pi / 2) - (angle_to_goal(curr_x, curr_y, goal_x, goal_y) - curr_theta)
+    # Solve for the angle of the right trangle between arc and normal or robot
+    a = dist_between(curr_x, curr_y, goal_x, goal_y) / 2
     h = a / math.cos(arc_triangle_theta)
-    #use trig to solve for hypotenous which is radius of the arc
+    # use trig to solve for hypotenous which is radius of the arc
     r = h
-    v = 0.22 #m/sec
-    omega = v/r
+    v = 0.22  # m/sec
+    omega = v / r
     return omega
+
 
 if __name__ == '__main__':
     Lab3().run()
